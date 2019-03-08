f50b9c29326d7bc51ee7e37688b163f3d54d8971
Nick Ritson <nritsonsa@gmail.com>
2019-03-08 15:13:22 +0000

    init

---
+++ src/components/MessageBoard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useAppContext } from './hooks';
+import CreateReaction from './CreateReaction';
+
+export default function MessageBoard() {
+    const { state: { messages } } = useAppContext();
+
+    return (
+        <div>
+            {
+                messages.map(messageItem => {
+                    const { id, text, username, timestamp } = messageItem;
+
+                    return (
+                        <div key={id}>
+                            <h4>{new Date(timestamp).toLocaleString()}</h4>
+                            <p>{text}</p>
+                            <h4>- {username}</h4>
+                            <CreateReaction />
+                            <hr />
+                        </div>
+                    );
+                })
+            }
+        </div>
+    )
+}
---
+++ src/components/PublishMessage.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { useAppContext } from './hooks';
+import { newMessage } from '../state/actions';
+
+export default function PublishMessage() {
+    const { state: { username }, pubsub: { publish } } = useAppContext();
+
+    const [text, setText] = useState('');
+
+    const updateText = event => {
+        setText(event.target.value);
+    }
+
+    const publishMessage = () => {
+        publish(newMessage({ text, username }));
+    }
+
+    const handleKeyPress = event => {
+        if (event.key === 'Enter') publishMessage();
+    }
+
+    return (
+        <div>
+            <h3>Got something to say?</h3>
+            <input value={text} onChange={updateText} onKeyPress={handleKeyPress} />
+            {' '}
+            <button onClick={publishMessage}>Publish it!</button>
+        </div>
+    )
+}
